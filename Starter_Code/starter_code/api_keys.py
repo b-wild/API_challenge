375b4afb052b914fd14d5c58571ae04595f79336
b-wild <bawilde79@gmail.com>
2023-02-04 18:10:14 -0500

    added

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = 81e184ad56e7e62e66a3e02d0a8dc0d4
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = 5bcaff5c13fd4df4b29b35f4d2a49b43
